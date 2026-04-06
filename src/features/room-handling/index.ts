5ec11ae7eea78cc533ed1676e52c265ffda84912
alexatsu <trevan98@gmail.com>
2026-04-06 15:45:31 +0300

    fix ci

--- src/features/room-handling/index.ts
+++ src/features/room-handling/index.ts
@@ -1,10 +1,9 @@
 import { commandsEvent } from '@/shared/lib/commands'
 
-import { createTempVCEvent, deleteEmptyTempVCEvent } from './auto-create-voice'
+import { createTempVCEvent } from './auto-create-voice'
 import { pingCommands } from './ping-current-voice'
 
 export function featureRoomHandler() {
     createTempVCEvent()
-    deleteEmptyTempVCEvent()
     commandsEvent(pingCommands)
 }
